7df89230d09e5120c6b1bd431d86d0d997496e7c
Busyreadingsomething <oro1919@gmail.com>
2018-02-08 10:51:08 -0800

    Implimented static serving and passed first server test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,12 +1,16 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var {serveAssets} = require ('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
   const { method, url, headers } = req;
-  console.log(method, url, headers);
   if (method === 'GET' && url === '/') {
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    serveAssets(res, archive.paths.index, (err, data) => {
+      res.writeHead(200, headers);
+      res.end(data);
+    });
+  } else {
+    res.end(archive.paths.list); 
   }
-  res.end(archive.paths.list);
 };
