84b4be16072c8eb747101899eb5eedcbc9fd2b9a
Busyreadingsomething <oro1919@gmail.com>
2018-02-08 16:29:22 -0800

    Implemented workerBee

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,21 @@
+var archive = require('../helpers/archive-helpers');
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+const workerBee = function() {
+  archive.readListOfUrls((err, data) => {
+    if (err) {
+      let badCall = `Logging Error: ${err}`;
+      fs.appendFile(exports.paths.workerLog, badCall, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        console.log('Error Logged');
+      });
+    }
+    data.pop();
+    console.log('--------->', data);
+    archive.downloadUrls(data);
+  });
+};
+
+workerBee();
